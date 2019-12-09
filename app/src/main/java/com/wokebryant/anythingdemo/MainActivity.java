097d9a5403d3efd01efab4b41c2e29b2a2c7aadc
wokebryant <luojia512@163.com>
2019-12-09 23:27:12 +0800

    add test

--- app/src/main/java/com/wokebryant/anythingdemo/MainActivity.java
+++ app/src/main/java/com/wokebryant/anythingdemo/MainActivity.java
@@ -10,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //test
+    //test again
+    //test again again
 }
