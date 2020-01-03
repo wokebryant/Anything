42c4fbca6ebf0aad8513217b9ec441a4209811d6
wb-lj589732 <wb-lj589732@alibaba-inc.com>
2020-01-03 20:44:58 +0800

    波纹进度条

--- app/src/main/java/com/wokebryant/anythingdemo/MainActivity.java
+++ app/src/main/java/com/wokebryant/anythingdemo/MainActivity.java
@@ -1,14 +1,68 @@
 package com.wokebryant.anythingdemo;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.wokebryant.anythingdemo.view.WaveProgressView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int UPDATE_PROGRESS = 1;
+    private static final int MAX_PROGRESS = 100;
+    private int mCurrentProgress = 0;
+    private WaveProgressView mWaveProgressView;
+    private Button mButton;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == UPDATE_PROGRESS) {
+                if (mCurrentProgress != MAX_PROGRESS) {
+                    mCurrentProgress += 1;
+                    mWaveProgressView.setProgress(mCurrentProgress);
+                    sendEmptyMessageDelayed(UPDATE_PROGRESS,100);
+                } else {
+                    mCurrentProgress = 0;
+                    mWaveProgressView.setProgress(0);
+                    mWaveProgressView.resetProgress();
+                    sendEmptyMessageDelayed(UPDATE_PROGRESS,100);
+                    //mWaveProgressView.release();
+                }
+
+            }
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mWaveProgressView = findViewById(R.id.waveProgressView);
+        mButton = findViewById(R.id.testBtn);
+
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.testBtn:
+                mHandler.sendEmptyMessageDelayed(UPDATE_PROGRESS,100);
+                //mWaveProgressView.setProgress(60);
+                break;
+            default:
+        }
     }
 
 }
---
+++ app/src/main/java/com/wokebryant/anythingdemo/view/WaveProgressView.java
@@ -0,0 +1,166 @@
+package com.wokebryant.anythingdemo.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class WaveProgressView extends View {
+    private int width;
+    private int height;
+
+    private Bitmap backgroundBitmap;
+
+    private Path mPath;
+    private Paint mPathPaint;
+
+    private float mWaveHeight = 10f;
+    private float mWaveWidth = 60f;
+    private String mWaveColor = "#FFFFFF";
+    private  int  mWaveSpeed = 30;
+
+    private int maxProgress = 100;
+    private int currentProgress = 0;
+    private float currentY;
+    private float currentMidY;
+
+    private float distance = 0;
+    private int RefreshGap = 10;
+
+    private static final int INVALIDATE = 0X777;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case INVALIDATE:
+                    invalidate();
+                    sendEmptyMessageDelayed(INVALIDATE,RefreshGap);
+                    break;
+            }
+        }
+    };
+
+    public WaveProgressView(Context context) {
+        this(context,null,0);
+    }
+    public WaveProgressView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    public WaveProgressView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        if(null==getBackground()){
+            throw new IllegalArgumentException(String.format("background is null."));
+        }else{
+            backgroundBitmap = getBitmapFromDrawable(getBackground());
+        }
+
+        mPath = new Path();
+        mPathPaint = new Paint();
+        mPathPaint.setAntiAlias(true);
+        mPathPaint.setStyle(Paint.Style.FILL);
+
+        handler.sendEmptyMessageDelayed(INVALIDATE,100);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        width = View.MeasureSpec.getSize(widthMeasureSpec);
+        currentY = height = MeasureSpec.getSize(heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if(backgroundBitmap!=null){
+            canvas.drawBitmap(createImage(), 0, 0, null);
+        }
+    }
+
+    private Bitmap createImage() {
+        mPathPaint.setColor(Color.parseColor(mWaveColor));
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        Bitmap bmp = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bmp);
+
+        currentMidY = height*(maxProgress-currentProgress)/maxProgress;
+        if(currentY > currentMidY){
+            currentY = currentY - (currentY-currentMidY)/10;
+        }
+        mPath.reset();
+        mPath.moveTo(0-distance,currentY);
+        int waveNum = width/((int)mWaveWidth);
+        int num = 0;
+        for(int i =0;i<waveNum;i++){
+            mPath.quadTo(mWaveWidth*(num+1)-distance,currentY-mWaveHeight,mWaveWidth*(num+2)-distance,currentY);
+            mPath.quadTo(mWaveWidth*(num+3)-distance,currentY+mWaveHeight,mWaveWidth*(num+4)-distance,currentY);
+            num+=4;
+        }
+        distance +=mWaveWidth/mWaveSpeed;
+        distance = distance%(mWaveWidth*4);
+        mPath.lineTo(width,height);
+        mPath.lineTo(0,height);
+        mPath.close();
+        canvas.drawPath(mPath, mPathPaint);
+        int min = Math.min(width,height);
+        backgroundBitmap = Bitmap.createScaledBitmap(backgroundBitmap,min,min,false);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP));
+
+        canvas.drawBitmap(backgroundBitmap,0,0,paint);
+        return bmp;
+    }
+
+    private Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        try {
+            Bitmap bitmap;
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (OutOfMemoryError e) {
+            return null;
+        }
+    }
+
+    public void setProgress(int currentProgress) {
+        this.currentProgress = currentProgress;
+    }
+    public void setWaveColor(String mWaveColor){
+        this.mWaveColor = mWaveColor;
+    }
+
+    public void resetProgress() {
+        currentY = height;
+    }
+
+    public void release() {
+        handler.removeCallbacksAndMessages(null);
+    }
+
+}
+
